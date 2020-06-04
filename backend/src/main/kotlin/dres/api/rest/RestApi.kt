b9749102eff6f1316d2751d561c70fd6883071db
Silvan Heller <silvan.heller@unibas.ch>
2020-06-04 11:21:34 +0200

    trying to serve from build with https

--- backend/src/main/kotlin/dres/api/rest/RestApi.kt
+++ backend/src/main/kotlin/dres/api/rest/RestApi.kt
@@ -107,7 +107,7 @@ object RestApi {
             it.accessManager(AccessManager::manage)
             it.addStaticFiles("html")
             it.addSinglePageRoot("/", "html/index.html")
-            it.enforceSsl = false
+            it.enforceSsl = true
         }.routes {
 
             path("api") {
